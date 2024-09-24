fa77b61a3e182c7de7398b9a76842f6cc44fb976
Siddharth <siddharth.060605@gmail.com>
2024-09-24 15:23:53 -0500

    Get very basic syntax working

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,87 @@
 use std::path::PathBuf;
 
-use clap::{Parser, Subcommand};
+use clap::{Parser, ValueEnum, Subcommand};
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Cli {
-    /// Optional name to operate on
-    name: Option<String>,
 
-    /// Sets a custom config file
-    #[arg(short, long, value_name = "FILE")]
-    config: Option<PathBuf>,
+    #[arg(long)]
+    host: Option<PathBuf>,
+
+    #[arg(long)]
+    port: Option<PathBuf>,
 
     /// Turn debugging information on
     #[arg(short, long, action = clap::ArgAction::Count)]
-    debug: u8,
+    verbose: u8,
+
+    #[clap(required = true)]
+    mode: Mode,
 
     #[command(subcommand)]
-    command: Option<Commands>,
+    command: Command
 }
 
 #[derive(Subcommand)]
-enum Commands {
-    /// does testing things
-    Test {
-        /// lists test values
-        #[arg(short, long)]
-        list: bool,
+enum Command {
+    /// Adds an object
+    Add {
+        object: Object,
     },
+
+    /// Removes an object
+    Remove {
+        /// Which type of object to remove
+        object: Object,
+        name: String
+    },
+
+    /// Lists objects
+    List {
+        /// List all, even inactive/disabled
+        object: Object,
+
+        #[arg(short, long)]
+        all: Option<bool> 
+    },
+
+    /// Edits an object
+    Edit {
+        object: Object
+    },
+
+    // Done messing with objects
+
+    /// Connect to server
+    Connect {
+        name: String
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum Mode {
+    Server,
+    Client
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum Object {
+    Nic,
+    User,
+    Hub,
 }
 
 fn main() {
     let cli = Cli::parse();
 
-    // You can check the value provided by positional arguments, or option arguments
-    if let Some(name) = cli.name.as_deref() {
-        println!("Value for name: {name}");
-    }
-
-    if let Some(config_path) = cli.config.as_deref() {
-        println!("Value for config: {}", config_path.display());
-    }
-
     // You can see how many times a particular flag or argument occurred
     // Note, only flags can have multiple occurrences
-    match cli.debug {
+    match cli.verbose {
         0 => println!("Debug mode is off"),
         1 => println!("Debug mode is kind of on"),
         2 => println!("Debug mode is on"),
         _ => println!("Don't be crazy"),
-    }
-
-    // You can check for the existence of subcommands, and if found use their
-    // matches just as you would the top level cmd
-    match &cli.command {
-        Some(Commands::Test { list }) => {
-            if *list {
-                println!("Printing testing lists...");
-            } else {
-                println!("Not printing testing lists...");
-            }
-        }
-        None => {}
     }
 
     // Continued program logic goes here...
